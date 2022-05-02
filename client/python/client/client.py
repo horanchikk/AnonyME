6e128ba936869cb1218f27407215bdf9b0a727ed
Ethosa <social.ethosa@gmail.com>
2022-05-02 15:30:52 +0700

    update python client

--- client/python/client/client.py
+++ client/python/client/client.py
@@ -3,6 +3,7 @@
 from typing import Dict, Any
 
 from requests import Session
+from .exceptions import handle_error
 
 
 class AnonymeClient:
@@ -10,12 +11,152 @@ class AnonymeClient:
             self,
             api_url: str = "http://localhost:8000/"
     ):
+        """Initializes AnonyME client
+
+        :param api_url: AnonyME API url"""
         self.session = Session()
         self.API_URL = api_url
+
+    def _send_req(
+            self,
+            url: str
+    ) -> Dict[str, Any]:
+        """Sends request to AnonyME API.
+
+        :param url: request URL."""
+        res = self.session.get(url).json()
+        error = handle_error(res)
+        if error:
+            raise error
+        return res
+
+    def users_get(
+            self,
+            token: str
+    ) -> Dict[str, Any]:
+        """Returns user by his token
+
+        :param token: user's token.
+
+        :raises AnonymeException: user isn't exists
+        """
+        return self._send_req(
+            f'{self.API_URL}users/get?token={token}'
+        )
 
     def users_get_all(
             self
     ) -> Dict[str, Any]:
-        return self.session.get(
+        """Returns all active users"""
+        return self._send_req(
             f'{self.API_URL}users/getall'
-        ).json()
+        )
+
+    def users_new(
+            self,
+            name: str
+    ) -> Dict[str, Any]:
+        """Creates new user with name
+
+        :param name: user's name
+
+        :raises AnonymeException: username is exists"""
+        return self._send_req(
+            f'{self.API_URL}users/new?name={name}'
+        )
+
+    def users_remove(
+            self,
+            token: str
+    ) -> Dict[str, Any]:
+        """Removes user by token
+
+        :param token: user's token
+
+        :raises AnonymeException: user isn't exists"""
+        return self._send_req(
+            f'{self.API_URL}users/remove?token={token}'
+        )
+
+    def users_messages_send(
+            self,
+            token: str,
+            text: str,
+            sticker_id: int
+    ) -> Dict[str, Any]:
+        """Sends message to room by user's token.
+
+        :param token: user's token
+        :param text: message text
+        :param sticker_id: sticker ID
+
+        :raises AnonymeException: user isn't exists; message text is empty; user hasn't room"""
+        return self._send_req(
+            f'{self.API_URL}users/messages.send?token={token}&text={text}&sticker_id={sticker_id}'
+        )
+
+    def users_room_enter(
+            self,
+            token: str,
+            room_token: str
+    ) -> Dict[str, Any]:
+        """Enter in room by token
+
+        :param token: user's token
+        :param room_token: room's token
+
+        :raises AnonymeException: user or room isn't exists; room is full"""
+        return self._send_req(
+            f'{self.API_URL}users/room.enter?token={token}&room_token={room_token}'
+        )
+
+    def users_room_leave(
+            self,
+            token: str
+    ) -> Dict[str, Any]:
+        """Leave from room by user's token
+
+        :param token: user's token
+
+        :raises AnonymeException: user or room isn't exists; room is full"""
+        return self._send_req(
+            f'{self.API_URL}users/room.leave?token={token}'
+        )
+
+    def rooms_get_all(
+            self
+    ) -> Dict[str, Any]:
+        """Returns all rooms"""
+        return self._send_req(
+            f'{self.API_URL}rooms/getall'
+        )
+
+    def rooms_get(
+            self,
+            token: str
+    ) -> Dict[str, Any]:
+        """Returns room by token
+
+        :param token: room's token
+
+        :raises AnonymeException: room isn't exists"""
+        return self._send_req(
+            f'{self.API_URL}rooms/get?token={token}'
+        )
+
+    def rooms_new(
+            self,
+            name: str,
+            user_token: str,
+            users_limit: int
+    ) -> Dict[str, Any]:
+        """Creates a new room by user's token
+
+        :param name: room's name
+        :param user_token: user's token
+        :param users_limit: room users limit
+
+        :raises AnonymeException: user isn't exists"""
+        return self._send_req(
+            f'{self.API_URL}rooms/new?name={name}&user_token={user_token}&users_limit={users_limit}'
+        )
--- client/python/tests/client_test.py
+++ client/python/tests/client_test.py
@@ -12,8 +12,27 @@ class Client(unittest.TestCase):
         )
 
     @staticmethod
-    def test_2_get_all_users():
+    def test_2_get_all_users_rooms():
         print(Client.client.users_get_all())
+        print(Client.client.rooms_get_all())
+
+    @staticmethod
+    def test_3_create_room_and_active():
+        username1 = 'ETHOSA2'
+        username2 = 'HORANCHIKK2'
+        user1 = Client.client.users_new(username1)
+        print('create user:', user1)
+        user2 = Client.client.users_new(username2)
+
+        room = Client.client.rooms_new('Ethosa room', user1['response']['token'], 4)
+        print('create room:', room)
+
+        response = Client.client.users_room_enter(user2['response']['token'], room['response']['token'])
+        print('enter in the room:', response)
+
+        response = Client.client.users_remove(user1['response']['token'])
+        Client.client.users_remove(user2['response']['token'])
+        print('delete users:', response)
 
 
 if __name__ == '__main__':
